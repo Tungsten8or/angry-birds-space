a5b50bbc44480d44419bbdc29d28c27bcfaf19c5
Tungsten8or <80408748+Tungsten8or@users.noreply.github.com>
2021-07-12 10:11:21 -0500

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,24 @@
+class Slingshot{
+constructor(bodyA,bodyB){
+    var options ={
+        bodyA:bodyA,
+        bodyB:bodyB,
+        length:100,
+        stiffness:0.04
+
+    }
+    this.sling = Matter.Constraint.create(options)
+    World.add(world,this.sling)
+}
+display(){
+    var pointA = this.sling.bodyA.position
+var pointB = this.sling.bodyB.position
+push ()
+strokeWeight(15)
+stroke ("purple")
+line (pointA.x,pointA.y,pointB.x,pointB.y)
+pop ()
+}
+
+
+}
